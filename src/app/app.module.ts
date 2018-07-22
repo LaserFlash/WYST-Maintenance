d2f91aca3525b7e3caf0fa895442cf5fb0e3dbc2
Bryn Bennett <bryn.t.bennett@gmail.com>
2018-07-22 18:10:43 +1200

    Readd service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -136,7 +136,7 @@ import 'hammerjs';
     MatStepperModule,
     CloudinaryModule.forRoot({Cloudinary}, { cloud_name: 'dhnh6uqep', upload_preset: 'oyywau4l' }),
     FileUploadModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [
     BoatUsageService,
