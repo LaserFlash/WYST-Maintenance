b38c0bf569b02d385a71fa2af9732f18791272b9
Bryn Bennett <bryn.t.bennett@gmail.com>
2019-05-13 12:02:29 +1200

    Generate version info at build time.
    Uses the provided version number in package.json and appends the 
    timestamp from when the build occurs.
    
    This is displayed in the side navigation with the source code link
    Close: #68

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  version: '0.0.0',
   firebase: {
     apiKey: "AIzaSyD-WLPK8SX993vT2KPnK3fa0KoffJzhlmc",
     authDomain: "wyst-maintenance.firebaseapp.com",
