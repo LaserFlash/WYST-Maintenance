bab33fc21e5ad98f4da280ef10cacd7c02ef7811
LaserFlash <bryn.t.bennett@gmail.com>
2020-11-22 19:28:32 +1300

    @angular/core migration - migration-v9-missing-injectable
    Missing @Injectable and incomplete provider definition migration. In Angular 9, enforcement of @Injectable decorators for DI is a bit stricter and incomplete provider definitions behave differently. Read more about this here: https://v9.angular.io/guide/migration-injectable

--- src/app/core/constants/menu-names/nameConversion.ts
+++ src/app/core/constants/menu-names/nameConversion.ts
@@ -1,5 +1,7 @@
 import { Levels } from './menuNames';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ImportanceConversionHelper {
   public static numberFromImportance(s) {
     return Levels.length - 1 - Levels.indexOf(s);
