af3b132063f1008a2dc6fce85406e5875e0f3259
LaserFlash <bryn.t.bennett@gmail.com>
2020-11-22 21:19:58 +1300

    CHORE(Minor fixes): minor fixes after major package updates

--- src/app/core/auth/authentication.service.ts
+++ src/app/core/auth/authentication.service.ts
@@ -1,17 +1,15 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 @Injectable()
 export class AuthenticationService {
-
   public authState: BehaviorSubject<boolean> = new BehaviorSubject(false);
   public isAdmin: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(private afAuth: AngularFireAuth, db: AngularFireDatabase) {
-    this.afAuth.authState.subscribe((data) => {
+    this.afAuth.authState.subscribe(async (data) => {
       console.log(data);
       if (!data) {
         this.isAdmin.next(false);
@@ -19,26 +17,28 @@ export class AuthenticationService {
       }
 
       if (data != null) {
-        this.afAuth.auth.currentUser.getIdTokenResult().then((idTokenResult) => {
-          this.authState.next(true); // Is now authenticated
+        (await this.afAuth.currentUser)
+          .getIdTokenResult()
+          .then((idTokenResult) => {
+            this.authState.next(true); // Is now authenticated
 
-          // Confirm the user is an Admin.
-          if (!!idTokenResult.claims.admin) {
-            this.isAdmin.next(true);
-            return;
-          }
+            // Confirm the user is an Admin.
+            if (!!idTokenResult.claims.admin) {
+              this.isAdmin.next(true);
+              return;
+            }
 
-          this.isAdmin.next(false); // If reached not an admin
-
-        }).catch(() => {
-          this.isAdmin.next(false);
-        });
+            this.isAdmin.next(false); // If reached not an admin
+          })
+          .catch(() => {
+            this.isAdmin.next(false);
+          });
       }
     });
   }
 
   /* Logout the current user */
-  public logout() {
-    this.afAuth.auth.signOut();
+  public async logout() {
+    await this.afAuth.signOut();
   }
 }
